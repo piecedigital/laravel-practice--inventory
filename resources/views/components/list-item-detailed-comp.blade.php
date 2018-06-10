1990005bd996992028b31870b990c9b86d437d01
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2018-06-09 20:40:34 -0400

    fixed styles and typos

--- resources/views/components/list-item-detailed-comp.blade.php
+++ resources/views/components/list-item-detailed-comp.blade.php
@@ -8,28 +8,44 @@
             <div class="single">
                 <img src="{{ $value->url }}" alt="">
             </div>
-            <div class="gallery">
-                <ul>
-                    @foreach ($media as $key => $media_data)
-                        <li class="">
-                            <img src="{{$media_data->url}}" alt="">
-                        </li>
-                    @endforeach
-                </ul>
+        </div>
+        <div class="wrapper">
+            <div class="separator"></div>
+            <div class="media">
+                <div class="gallery">
+                    <ul>
+                        @foreach ($media as $key => $media_data)
+                            <li class="">
+                                <img src="{{$media_data->url}}" alt="">
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div class="separator"></div>
-        <div class="descriptions">{{ $value->description }}</div>
-        <div class="metadata">
-            <div class="meta-width"><h3>width</h3>: {{ $value->dimensions_width_value . "" . $value->dimensions_width_unit }}</div>
-            <div class="meta-height"><h3>height</h3>: {{ $value->dimensions_height_value . "" . $value->dimensions_height_unit }}</div>
-            <div class="meta-lenth"><h3>lenth</h3>: {{ $value->dimensions_lenth_value . "" . $value->dimensions_lenth_unit }}</div>
-            <div class="meta-weight"><h3>weight</h3>: {{ $value->weight_value . "" . $value->weight_unit }}</div>
-            <div class="meta-color"><h3>color</h3>: {{ $value->color }}</div>
-        </div>
-        <div class="separator"></div>
-        <div class="">
-            <a href="/inventory-reviews/{{ $value->id }}">See Reviews</a>
+            <div class="separator"></div>
+            <div class="split-box">
+                <div class="split-content descriptions">
+                    Sizeable description of the product. Sizeable description of the product. Sizeable description of the product. Sizeable description of the product. Sizeable description of the product. Sizeable description of the product.
+                    {{-- {{ $value->description }} --}}
+                </div>
+                <div class="splitter"></div>
+                <div class="split-content metadata">
+                    <div class="column">
+                        <div class="meta-width"><h3>Width:</h3> {{ $value->dimensions_width_value . "" . $value->dimensions_width_unit }}</div>
+                        <div class="meta-height"><h3>Height:</h3> {{ $value->dimensions_height_value . "" . $value->dimensions_height_unit }}</div>
+                        <div class="meta-lenth"><h3>Length:</h3> {{ $value->dimensions_length_value . "" . $value->dimensions_length_unit }}</div>
+                    </div>
+                    <div class="column">
+                        <div class="meta-weight"><h3>Weight:</h3> {{ $value->weight_value . "" . $value->weight_unit }}</div>
+                        <div class="meta-color"><h3>Color:</h3> {{ $value->color }}</div>
+                        <div class="meta-color"><h3>Price:</h3> {{ $value->price }}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="separator"></div>
+            <div class="">
+                <a href="/inventory-reviews/{{ $value->id }}">See Reviews</a>
+            </div>
         </div>
     </div>
 </div>
